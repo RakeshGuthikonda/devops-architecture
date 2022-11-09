e4cc96c575feaa310867d5ca247896badaa19f19
RakeshGuthikonda <86244611+RakeshGuthikonda@users.noreply.github.com>
2022-11-09 17:12:10 +0530

    Update main.tf

--- terraform/environments/master/main.tf
+++ terraform/environments/master/main.tf
@@ -1,14 +1,40 @@
 provider "google" {
   region      = "${var.region}"
+  zone="us-central1-c"
 }
 
-resource "google_project" "giridhar_project-2" {
-  name       = "giridhar-project-2"
-  project_id = "giridhar-project-2"
+resource "google_project" "giridhar_project-3" {
+  name       = "giridhar-project-3"
+  project_id = "giridhar-project-3"
 
   org_id = "${var.gsuite_org_id}"
   billing_account = "${var.billing_account_id}"
 }
+
+
+
+# Create a single Compute Engine instance
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  zone         = "us-central1-c"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+  allow_stopping_for_update = "true"
+}
+
+
+
+
 
 # resource "google_project_iam_binding" "gsuite_project_owner" {
 #   project = "${google_project.giridhar_project-2.project_id}"
@@ -18,40 +44,4 @@ resource "google_project" "giridhar_project-2" {
 #     "user:${var.gsuite_user}",
 #     "user:${var.billing_account_user}",
 #   ]
-# }
-
-
-# resource "google_billing_account_iam_binding" "editor" {
-#   billing_account_id = "${var.billing_account_id}"
-#   role               = "roles/billing.user"
-#   members = [
-#     "user:banothuramu@gnimmagadda.com",
-#   ]
-# }
-
-
-
-# provider "google" {
-#     project ="giridhar-project-1"
-#     region="us-central1"
-#     zone="us-central1-c"
-# }
-
-# # Create a single Compute Engine instance
-# resource "google_compute_instance" "vm_instance" {
-#   name         = "terraform-instance"
-#   machine_type = "e2-micro"
-#   zone         = "us-central1-c"
-#   tags         = ["ssh"]
-
-#   boot_disk {
-#     initialize_params {
-#       image = "centos-cloud/centos-7"
-#     }
-#   }
-
-#   network_interface {
-#     network = "default"
-#   }
-#   allow_stopping_for_update = "true"
 # }
