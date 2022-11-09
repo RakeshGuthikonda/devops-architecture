0d754946edf90be40396be41b7badbf9d65eb38c
RakeshGuthikonda <86244611+RakeshGuthikonda@users.noreply.github.com>
2022-11-09 12:54:43 +0530

    Update main.tf

--- terraform/environments/master/main.tf
+++ terraform/environments/master/main.tf
@@ -1,24 +1,24 @@
-provider "google" {
-  region      = "${var.region}"
-}
+# provider "google" {
+#   region      = "${var.region}"
+# }
 
-resource "google_project" "test_project" {
-  name       = "test-project"
-  project_id = "test-project-1"
+# resource "google_project" "test_project" {
+#   name       = "test-project"
+#   project_id = "test-project-1"
 
-  org_id = "${var.gsuite_org_id}"
-  billing_account = "${var.billing_account_id}"
-}
+#   org_id = "${var.gsuite_org_id}"
+#   billing_account = "${var.billing_account_id}"
+# }
 
-resource "google_project_iam_binding" "gsuite_project_owner" {
-  project = "${google_project.test_project.project_id}"
-  role    = "roles/owner"
+# resource "google_project_iam_binding" "gsuite_project_owner" {
+#   project = "${google_project.test_project.project_id}"
+#   role    = "roles/owner"
 
-  members = [
-    "user:${var.gsuite_user}",
-    "user:${var.billing_account_user}",
-  ]
-}
+#   members = [
+#     "user:${var.gsuite_user}",
+#     "user:${var.billing_account_user}",
+#   ]
+# }
 
 
 # resource "google_billing_account_iam_binding" "editor" {
@@ -28,3 +28,30 @@ resource "google_project_iam_binding" "gsuite_project_owner" {
 #     "user:banothuramu@gnimmagadda.com",
 #   ]
 # }
+
+
+
+provider "google" {
+    project ="giridhar-project-1"
+    region="us-central1"
+    zone="us-central1-c"
+}
+
+# Create a single Compute Engine instance
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  zone         = "us-central1-c"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+  allow_stopping_for_update = "true"
+}
