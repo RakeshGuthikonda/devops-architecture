e32d81fed8c8a2765243504b5fb379485171edf3
RakeshGuthikonda <86244611+RakeshGuthikonda@users.noreply.github.com>
2022-11-10 17:24:02 +0530

    Add files via upload

---
+++ terraform/environments/master/modules/global/variables.tf
@@ -0,0 +1,13 @@
+variable "project_id" {
+    default = "giridhar-project-1"
+}
+variable "region" {
+    default = "us-central1"
+}
+variable "zone" {
+    default = "us-central1-c"
+}
+
+variable "subnet_name"{
+    default = "subnet-01"
+}
