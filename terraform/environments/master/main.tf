50ccef9d92dfa3e819c0c45a3df56f7827b5dbf1
RakeshGuthikonda <86244611+RakeshGuthikonda@users.noreply.github.com>
2022-11-11 15:28:02 +0530

    Update main.tf

--- terraform/environments/master/main.tf
+++ terraform/environments/master/main.tf
@@ -11,13 +11,13 @@ provider "google"{
     zone = var.zone
 }
 
-# resource "google_project" "test_project" {
-#   name       = "test-project"
-#   project_id = var.project_id
+resource "google_project" "test_project" {
+  name       = "test-project"
+  project_id = var.project_id
 
-#   org_id = "${var.gsuite_org_id}"
-#   billing_account = "${var.billing_account_id}"
-# }
+  org_id = "${var.gsuite_org_id}"
+  billing_account = "${var.billing_account_id}"
+}
 
 resource "google_project_service" "project" {
   project = var.project_id
@@ -44,12 +44,13 @@ resource "google_project_service" "project" {
 
 
 
-# module "subnetwork"{
-#     source = "./modules/global"
-# }
-# module "vpc"{
-#     source = "./modules/global"
-# }
+module "subnetwork"{
+    source = "./modules/global"
+}
+module "vpc"{
+    source = "./modules/global"
+}
+    
 module "vm-instance"{
     source = "./modules/global"
      depends_on = [google_project_service.project]
