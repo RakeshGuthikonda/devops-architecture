c1ee801ffe758feaf91b281c9121407a828f7785
RakeshGuthikonda <86244611+RakeshGuthikonda@users.noreply.github.com>
2022-11-11 13:09:43 +0530

    Update main.tf

--- terraform/environments/master/main.tf
+++ terraform/environments/master/main.tf
@@ -23,8 +23,8 @@ resource "google_project_service" "project" {
   project = var.project_id
   service   = "compute.googleapis.com"
     timeouts {
-    create = "5m"
-    update = "5m"
+    create = "30m"
+    update = "40m"
   }
 
   disable_dependent_services = true
