8a65b02ddfa7cbc88b6ce3fe0f61844e5e16467f
RakeshGuthikonda <86244611+RakeshGuthikonda@users.noreply.github.com>
2022-11-11 16:32:05 +0530

    Update main.tf

--- terraform/environments/master/main.tf
+++ terraform/environments/master/main.tf
@@ -11,34 +11,33 @@ provider "google"{
     zone = var.zone
 }
 
-resource "google_project" "test_project17" {
-  name       = "test-project17"
-  project_id = var.project_id
+# resource "google_project" "test_project17" {
+#   name       = "test-project17"
+#   project_id = var.project_id
 
-  org_id = "${var.gsuite_org_id}"
-  billing_account = "${var.billing_account_id}"
-}
+#   org_id = "${var.gsuite_org_id}"
+#   billing_account = "${var.billing_account_id}"
+# }
 
-resource "google_project_service" "project" {
+resource "google_project_service" "compute" {
   project = var.project_id
   service   = "compute.googleapis.com"
-#     timeouts {
-#     create = "30m"
-#     update = "40m"
-#   }
 
   disable_dependent_services = false
-  depends_on = [google_project.test_project17]
+#   depends_on = [google_project.test_project17]
+}
+
+resource "google_project_service" "firewall" {
+  project = var.project_id
+  service   = "vpcaccess.googleapis.com"
+
+
+  disable_dependent_services = false
+#   depends_on = [google_project.test_project17]
 }
 
 
 
-
-# resource "time_sleep" "wait_30_seconds" {
-#   depends_on = [null_resource.previous]
-
-#   create_duration = "2m"
-# }
 
 
 
@@ -47,15 +46,15 @@ resource "google_project_service" "project" {
 
 module "subnetwork"{
     source = "./modules/global"
-    depends_on = [google_project_service.project]
+    depends_on = [google_project_service.firewall]
 }
 module "vpc"{
     source = "./modules/global"
-    depends_on = [google_project_service.project]
+    depends_on = [google_project_service.firewall]
 }
     
 module "vm-instance"{
     source = "./modules/global"
-     depends_on = [google_project_service.project]
+     depends_on = [google_project_service.compute]
 
 }
